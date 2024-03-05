86bbc1ef17447ace57af1475147b734c6eb496eb
Raqueline Gomes <55905389+raquelinevr@users.noreply.github.com>
2024-03-05 14:31:00 -0300

    Update Aula2-OO.kt

--- src/main/kotlin/Aula2-OO.kt
+++ src/main/kotlin/Aula2-OO.kt
@@ -3,29 +3,36 @@ package br.ifpb.pdm
 fun main() {
     val repositorioAnimal = RepositorioAnimal()
     var opcao = 0
-    while (opcao != 6) {
+    while (opcao != 10) {
         menu()
         print("Digite a opção: ")
         opcao = readlnOrNull()?.toInt() ?: 0
         when (opcao) {
-            0 -> {
-                val animal = Animal(10)
-                animal.nome = "genérico"
-                repositorioAnimal.adicionar(animal)
-            }
             1 -> {
-                val cachorro = Cachorro(10)
-                cachorro.nome = "Rex"
+                print("Digite o nome do cachorro: ")
+                val nomeCachorro = readln()
+                print("Digite a idade do cachorro: ")
+                val idadeCachorro = readln()?.toInt() ?: 0
+                val cachorro = Cachorro(idadeCachorro, Cor.PRETO)
+                cachorro.nome = nomeCachorro ?: ""
                 repositorioAnimal.adicionar(cachorro)
             }
             2 -> {
-                val gato = Gato(5)
-                gato.nome = "Felix"
+                print("Digite o nome do gato: ")
+                val nomeGato = readln()
+                print("Digite a idade do gato: ")
+                val idadeGato = readln()?.toInt() ?: 0
+                val gato = Gato(idadeGato, Cor.BRANCO)
+                gato.nome = nomeGato ?: ""
                 repositorioAnimal.adicionar(gato)
             }
             3 -> {
-                val passaro = Passaro(2)
-                passaro.nome = "Piu"
+                print("Digite o nome do pássaro: ")
+                val nomePassaro = readln()
+                print("Digite a idade do pássaro: ")
+                val idadePassaro = readln()?.toInt() ?: 0
+                val passaro = Passaro(idadePassaro, Cor.AMARELO)
+                passaro.nome = nomePassaro ?: ""
                 repositorioAnimal.adicionar(passaro)
             }
             4 -> {
@@ -35,24 +42,58 @@ fun main() {
                 repositorioAnimal.animais.forEach(Animal::emitirSom)
                 repositorioAnimal.animais.forEach { it.emitirSom()}
             }
+            6 -> {
+                print("Digite o nome do animal que será removido: ")
+                val nomeRemover = readlnOrNull() ?: ""
+                repositorioAnimal.remover(nomeRemover)
+            }
+            7 -> {
+                print("Digite a cor dos animais a serem listados: ")
+                println("Animais de cor BRANCA: ")
+                repositorioAnimal.listarAnimaisPorCor(Cor.BRANCO)
+            }
+
+            8 -> {
+                print("Digite a idade dos animais a serem listados: ")
+                println("Animais com a idade 10: ")
+                repositorioAnimal.listarAnimaisPorIdade(10)
+            }
+
+            9 -> {
+                
+                print("Digite o nome completo da pessoa: ")
+                val nomeCompleto = readln()
+                print("Digite a idade da pessoa: ")
+                val idadePessoa = readln()?.toInt() ?: 0
+                val pessoa = Homem(idadePessoa, nomeCompleto ?: "")
+                repositorioAnimal.adicionar(pessoa)
+            }
+
         }
 
     }
 }
 
-open class Animal(var idade: Int) {
+enum class Cor{
+    BRANCO, PRETO, AMARELO
+}
+
+
+abstract class Animal(var idade: Int, var cor: Cor) {
     open var nome: String = ""
         get() = "Animal: $field"
         set(valor) {
             field = valor
         }
 
-    open fun emitirSom() {
-        println("Som de animal")
+    abstract fun emitirSom()
+
+    open fun idadeEmAnosHumanos(): Int{
+        return idade * 7
     }
 }
 
-class Cachorro(idade: Int) : Animal(idade) {
+class Cachorro(idade: Int, cor: Cor) : Animal(idade, cor) {
     override var nome: String = ""
         get() = field
         set(valor) {
@@ -62,26 +103,38 @@ class Cachorro(idade: Int) : Animal(idade) {
         println("Au au")
     }
 }
-class Gato(idade: Int) : Animal(idade) {
+class Gato(idade: Int, cor: Cor) : Animal(idade, cor) {
     override fun emitirSom() {
         println("Miau")
     }
 }
 
-class Passaro(idade: Int) : Animal(idade) {
+class Passaro(idade: Int, cor: Cor) : Animal(idade, cor) {
     override fun emitirSom() {
         println("Piu piu")
     }
 }
 
+class Homem(idade: Int, var nomeCompleto: String) : Animal(idade, Cor.BRANCO) {
+    override var nome: String = ""
+
+    override fun emitirSom() {
+        println("Som de humano")
+    }
+
+    override fun idadeEmAnosHumanos(): Int {
+        return idade
+    }
+}
+
 fun menu() {
-    println("0 - Animal")
     println("1 - Cachorro")
     println("2 - Gato")
     println("3 - Pássaro")
     println("4 - Listar Animais")
     println("5 - Emitir som")
-    println("6 - Sair")
+    println("6 - Remover um animal ")
+    println("111 - Sair")
 }
 
 class RepositorioAnimal {
@@ -94,5 +147,39 @@ class RepositorioAnimal {
     fun listar() {
         animais.forEach { println(it.nome) }
     }
-}
 
+    fun buscarNome(nome: String): Animal? {
+        return animais.find {it.nome == nome }
+    }
+
+    fun remover(nomeRemover: String){
+        val animalRemover = buscarNome(nomeRemover)
+        if (animalRemover != null){
+            animais.remove(animalRemover)
+            print("Animal removido com sucesso: $animalRemover")
+        } else{
+            println("Animal não encontrado!")
+        }
+    }
+
+
+    fun listarAnimaisPorCor(cor: Cor){
+        val animalCor = animais.filter { it.cor == cor }
+        if (animalCor.isEmpty()){
+            println("Não foram encontrados animais com a cor informada. ")
+        } else{
+            println("Cor não encontrada. ")
+        }
+    }
+
+    fun listarAnimaisPorIdade(idade: Int) {
+        val animalIdade = animais.filter { it.idade == idade }
+        if (animalIdade.isEmpty()) {
+            println("Não foram encotrados animais com a idade $idade")
+        } else{
+            println("Idade não encontrada. ")
+        }
+
+
+    }
+}
